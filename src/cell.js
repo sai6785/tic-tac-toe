6adfb9289fccfe347d9a35dbc95f17793429e1cc
sai6785 <50537184+sai6785@users.noreply.github.com>
2019-05-15 21:12:05 +0530

    Created Cell component with sample content.

---
+++ src/cell.js
@@ -0,0 +1,17 @@
+import React, { Component, Fragment } from 'react';
+
+class Cell extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <div>
+                Render Cell box here.
+            </div>
+         );
+    }
+}
+ 
+export default Cell;
